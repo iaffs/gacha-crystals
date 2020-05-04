6a64a33bdb9a9aefa77b07d8ff2009ed8dfefad8
iaffs <byiaffs@gmail.com>
2020-05-04 02:09:20 +0200

    final cleanup ws-handler.js

--- src/server/ws-handler.js
+++ src/server/ws-handler.js
@@ -1,30 +1,29 @@
-const express_ws = require('express-ws');
+// copied from https://github.com/arcuri82/web_development_and_api_design/blob/master/exercise-solutions/quiz-game/part-09/src/server/ws-handler.js
 
+const express_ws = require("express-ws");
 
 let ews;
 
 function init(app) {
+  ews = express_ws(app);
 
-    ews = express_ws(app);
+  app.ws("/", function (socket, req) {
+    console.log("Established a new WS connection");
 
-    app.ws('/', function (socket, req) {
-        console.log('Established a new WS connection');
-
-
-        socket.on('message', (data) => {
-            processWebsocketMessage(data);
-         });
-
-        //close is treated specially
-        socket.on('close', () => {
-           console.log('connected closed');
-        });
+    socket.on("message", (data) => {
+      processWebsocketMessage(data);
     });
+
+    socket.on("close", () => {
+      console.log("connected closed");
+    });
+  });
 }
 
+/*
 function processWebsocketMessage() {
-   // process free gift here
+  // process free gift here
 }
+*/
 
-
-module.exports = {init};
+module.exports = { init };
